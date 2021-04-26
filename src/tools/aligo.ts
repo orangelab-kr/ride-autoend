5ad52e2e08a48f165775596d5fe01d0826307305
bakjonghun <bakjonghun@hikick.kr>
2021-04-26 20:01:42 +0900

    add: alimtalk

--- src/tools/aligo.ts
+++ src/tools/aligo.ts
@@ -6,35 +6,79 @@ const engine = new Liquid({
   extname: '.liquid',
 });
 
+export let token: string | null;
+
+export async function getToken(
+  proxy: string,
+  userId: string,
+  apiKey: string,
+  expiry = 1
+): Promise<string | null> {
+  try {
+    if (token) return token;
+    const res = await rp({
+      method: 'POST',
+      proxy,
+      uri: `https://kakaoapi.aligo.in/akv10/token/create/${expiry}/h`,
+      formData: { apikey: apiKey, userid: userId },
+      json: true,
+    });
+
+    if (res.code !== 0) throw Error(`오류가 발생하였습니다.`);
+    if (!res.token) throw Error('서버에서 토큰을 반환하지 않았습니다.');
+    setTimeout(() => (token = null), expiry * 3600 * 900);
+    token = res.token;
+    return token;
+  } catch (err) {
+    console.log(err);
+    throw Error(`서버에서 잘못된 값을 반환하였습니다.`);
+  }
+}
+
 export async function send(
   phone: string,
   template: string,
+  title: string,
   props: any
-): Promise<boolean> {
+): Promise<void> {
   const {
     ALIGO_PROXY,
     ALIGO_IDENTIFIER,
+    ALIGO_SENDER_KEY,
     ALIGO_SECRET,
     ALIGO_SENDER,
   } = process.env;
-  if (!ALIGO_PROXY || !ALIGO_IDENTIFIER || !ALIGO_SECRET || !ALIGO_SENDER) {
+  if (
+    !ALIGO_PROXY ||
+    !ALIGO_SENDER_KEY ||
+    !ALIGO_IDENTIFIER ||
+    !ALIGO_SECRET ||
+    !ALIGO_SENDER
+  ) {
     throw Error('문자를 발송할 수 없습니다.');
   }
 
+  const token = await getToken(ALIGO_PROXY, ALIGO_IDENTIFIER, ALIGO_SECRET);
   const renderer = await engine.renderFile(template, props);
-  const res = await rp({
+  await rp({
     method: 'POST',
-    url: 'http://apis.aligo.in',
+    url: 'http://kakaoapi.aligo.in/akv10/alimtalk/send/',
     proxy: ALIGO_PROXY,
     json: true,
     formData: {
-      user_id: ALIGO_IDENTIFIER,
-      key: ALIGO_SECRET,
+      apikey: ALIGO_SECRET,
+      userid: ALIGO_IDENTIFIER,
+      token,
+      senderkey: ALIGO_SENDER_KEY,
+      tpl_code: template,
       sender: ALIGO_SENDER,
-      receiver: `0${phone.substr(3)}`,
-      msg: renderer,
+      receiver_1: `0${phone.substr(3)}`,
+      subject_1: title,
+      failover: 'Y',
+      message_1: renderer,
+      fsubject_1: title,
+      fmessage_1: renderer,
       testmode_yn: process.env.NODE_ENV !== 'prod' ? 'true' : 'false',
     },
   });
-  return res.result_code === 1;
 }
