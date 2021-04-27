8e11ccb9949f33761a4349bfd0a3dc01b19a216a
bakjonghun <bakjonghun@hikick.kr>
2021-04-27 18:02:44 +0900

    add: internal client

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,6 +1,7 @@
 export * from './aligo';
 export * from './firestore';
 export * from './iamport';
+export * from './internalClient';
 export * from './logger';
 export * from './price';
 export * from './webhook';
